16ceaf7bbd537ec53f751bf09621c9d922ececaa
Shalini8 <shalu8mar@gmail.com>
2021-04-27 15:58:00 +0530

    [Modified] generic Class with 3 parameters is used

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -1,7 +1,17 @@
 package com.bridgelabz;
-public class Maximum <E extends Comparable> {
 
-        public static <E extends Comparable> E testMax(E x, E y, E z) {
+public class Maximum <T extends Comparable <T>>{
+    T x,y,z;
+
+    public Maximum() {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public T testMax(){
+        return Maximum.testMax(x,y,z);
+    }
+    public static <E extends Comparable> E testMax(E x, E y, E z) {
             if (x.compareTo(y) > 0) {
                 if (x.compareTo(z) > 0) {
                     return x;
