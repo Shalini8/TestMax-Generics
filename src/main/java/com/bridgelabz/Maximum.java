56d0f5b3e34eca65a7fdf6d134756ec3c3c45aa9
Shalini8 <shalu8mar@gmail.com>
2021-04-27 08:14:48 +0530

    Added float value to testmax

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -9,4 +9,13 @@ public class Maximum {
         }
     return max;
     }
+    public Float testMax(Float x, Float y, Float z) {
+        Float max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
+        }if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        return max;
+    }
 }
--- src/test/java/com/bridgelabz/TestMaximum.java
+++ src/test/java/com/bridgelabz/TestMaximum.java
@@ -24,6 +24,27 @@ public class TestMaximum {
         int valueToCheck = maximum.testMax(5,3,8);
         Assert.assertEquals(8, valueToCheck);
     }
+    @Test
+    public void checkMax_Of_Float_AtFirstPosition() {
+        Maximum maximum = new Maximum();
+        float valueToCheck = maximum.testMax(5.0f,3.0f,4.0f);
+        Assert.assertEquals(5.0f, valueToCheck,0.0);
+    }
+
+    @Test
+    public void checkMax_Of_Float_AtSecondPosition() {
+        Maximum maximum = new Maximum();
+        float valueToCheck = maximum.testMax(5.0f,8.0f,4.0f);
+        Assert.assertEquals(8.0, valueToCheck,0.0);
+    }
+
+    @Test
+    public void checkMax_Of_Float_AtThirdPosition() {
+        Maximum maximum = new Maximum();
+        float valueToCheck = maximum.testMax(5.0f,3.0f,8.0f);
+        Assert.assertEquals(8.0, valueToCheck,0.0);
+    }
+
 
 
 }
